0055bbb41f7ea8c46f822d0b3008ec9b892faefa
Andrea Calero Caro <alu0101202952@ull.edu.es>
2021-03-11 19:18:54 +0000

    Fallo ejercicio-1.spec.ts

---
+++ practica5_clases/tests/ejercicio-1.spec.ts
@@ -0,0 +1,8 @@
+import 'mocha';
+import {expect} from 'chai';
+import {pokedex} from '../src/ejercicio-1';
+
+describe('pokedex', () => {
+  it('pokedex("serperior") returns name, weight and height, type, stats', () => {
+    expect(new pokedex("serperior", 63.0, 3.3, "grass",(attack, defense, speed, hp) => [75, 95, 113, 75])).to.be.equal();
+  });
