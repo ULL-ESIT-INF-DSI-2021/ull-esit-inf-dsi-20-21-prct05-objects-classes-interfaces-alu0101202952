be44152e367dff0a74bf898ba034d32408de246f
Andrea Calero Caro <alu0101202952@ull.edu.es>
2021-03-12 15:42:03 +0000

    pull

--- practica5_clases/tests/ejercicio-1.spec.ts
+++ practica5_clases/tests/ejercicio-1.spec.ts
@@ -2,7 +2,8 @@ import 'mocha';
 import {expect} from 'chai';
 import {pokedex} from '../src/ejercicio-1';
 
+
 describe('pokedex', () => {
   it('pokedex("serperior") returns name, weight and height, type, stats', () => {
-    expect(new pokedex("serperior", 63.0, 3.3, "grass",(attack, defense, speed, hp) => [75, 95, 113, 75])).to.be.equal();
-  });
+    expect(new pokedex("serperior", 63.0, 3.3, "grass",(attack, defense, speed, hp) => [75, 95, 113, 75])).to.be.equal(("serperior"));
+  })});
