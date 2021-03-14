7fd33f35e3b8691df878b7b5e2832dc4d1324ecf
Andrea Calero Caro <alu0101202952@ull.edu.es>
2021-03-14 16:54:21 +0000

    Fallo test devolver modelo guagua class VehiclePublic

--- practica5_clases/tests/ejercicio-3.spec.ts
+++ practica5_clases/tests/ejercicio-3.spec.ts
@@ -11,19 +11,15 @@ describe('VehiclePrivate', () => {
   it('vehicle1.getMarca() returns marca of vehicle1', () => {
     return expect(vehicle1.getMarca()).to.be.equal("Seat");
   })
-
   it('vehicle1.getModelo() returns modelo of vehicle1', () => {
     return expect(vehicle1.getModelo()).to.be.equal("Ibiza");
   })
-
   it('vehicle1.getAntigüdad() returns antigüedad of vehicle1', () => {
     return expect(vehicle1.getAntiguedad()).to.be.equal(5);
   })
-
   it('vehicle1.getColor() returns color of vehicle1', () => {
     return expect(vehicle1.getColor()).to.be.equal("azul");
   })
-
   it('vehicle1.getTipo() returns tipo of vehicle1', () => {
     return expect(vehicle1.getTipo()).to.be.equal("turismo");
   })
@@ -32,5 +28,8 @@ describe('VehiclePrivate', () => {
 describe('VehiclePublic', () => {
   it('titsa.getMarca() returns marca of titsa guagua', () => {
     return expect(titsa.getMarca()).to.be.equal("Scania");
+  })
+  it('titsa.getModelo() returns modelo of titsa guagua', () => {
+    return expect(titsa.getModelo()).to.be.equal("K380");
   })
 });
