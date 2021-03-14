efc4312312402ed6c6aef77f107d9ca04d4bdac0
Andrea Calero Caro <alu0101202952@ull.edu.es>
2021-03-14 13:22:33 +0000

    MEDIO Acierto test funcion referenceAPAWDOI() referencia del articulo class BM

--- practica5_clases/src/index.ts
+++ practica5_clases/src/index.ts
@@ -241,6 +241,17 @@ export class Pokedex{
     getCitations(){
       return this.countCitations;
     }
+
+    public referenceAPAWDOI() {
+      /**
+       * formato APA sin DOI:
+       * Apellido [coma] inicial (es) del nombre del autor [punto] año entre paréntesis [punto] título del articulo [punto] título de
+       * la Revista en cursivas [coma] volumen (número entre paréntesis) [coma] la referencia a las páginas [punto]. Recuperado DE 
+       * [insertar http: y el link]
+       */
+
+      console.log(`Dahmani, I., Hifi, M., ${article1.getTitle}, Ann Oper Res 298, 125–147 (2021). https://doi.org/10.1007/s10479-019-03290-3`);
+    }
   } //class BM ends
 
 
@@ -255,4 +266,5 @@ export class Pokedex{
     console.log(`Abstract: ${article.getAbstract()}\n`);
     console.log(`Editorial: ${article.getEditorial()}\n`);
     console.log(`Citations: ${article.getCitations()}\n`);
+    console.log(`\nReference APA formar without DOI: ${article.referenceAPAWDOI()}\n`);
  });   
