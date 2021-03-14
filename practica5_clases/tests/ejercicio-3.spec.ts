9d28708d977c344386e49c6e12a87d13d071409a
Andrea Calero Caro <alu0101202952@ull.edu.es>
2021-03-14 16:18:13 +0000

    Fallo test devolver antiguedad vehículo privado class VehiclesPrivate

--- practica5_clases/tests/ejercicio-3.spec.ts
+++ practica5_clases/tests/ejercicio-3.spec.ts
@@ -14,4 +14,8 @@ describe('VehiclePrivate', () => {
   it('vehicle1.getModelo() returns modelo of vehicle1', () => {
     return expect(vehicle1.getModelo()).to.be.equal("Ibiza");
   })
+
+  it('vehicle1.getAntigüdad() returns antigüedad of vehicle1', () => {
+    return expect(vehicle1.getAntiguedad()).to.be.equal(5);
+  })
 });
