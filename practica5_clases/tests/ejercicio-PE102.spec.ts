0120e354dfaf561b6183ef8d143138215b2dd674
Andrea Calero Caro <alu0101202952@ull.edu.es>
2021-03-15 10:52:27 +0000

    Fallo test multiplicacion de  racional clss Rational

--- practica5_clases/tests/ejercicio-PE102.spec.ts
+++ practica5_clases/tests/ejercicio-PE102.spec.ts
@@ -11,25 +11,35 @@ describe('Rational', () => {
     it('rartional1.getNumerador() returns numerador del racional', () => {
       return expect(rational1.getNumerador()).to.be.equal(4);
     })
+    
     it('rartional1.getDenominador() returns denominador del racional', () => {
         return expect(rational1.getDenominador()).to.be.equal(8);
     })
     it('rartional1.setNumerador() asigna el numerador a la propiedad numerador de racional', () => {
         return expect(rational1.setNumerador(1)).to.be.equal(undefined);
     })
+
     it('rartional1.setDenominador() asigna el Denominador a la propiedad numerador de racional', () => {
         return expect(rational1.setDenominador(2)).to.be.equal(undefined);
     })
+
     it('rartional1.simplificacion(4,8) simplifica el racional', () => {
         return expect(rational1.simplificacion(4,8)).to.be.equal(undefined);
     })
+
     it('rartional2.inverso(3,5) muestra el inverso del racional', () => {
         return expect(rational2.inverso(3,5)).to.be.equal(undefined);
     })
+
     it('rartional23.suma(3,5,1,2) muestra la suma de racionales', () => {
         return expect(rational2.suma(3,5,1,2)).to.be.equal(undefined);
     })
+
     it('rartional23.resta(3,5,1,2) muestra la resta de racionales', () => {
         return expect(rational2.resta(3,5,1,2)).to.be.equal(undefined);
+    })
+
+    it('rartional23.multiplicacion(3,5,1,2) muestra la multiplicación de racionales', () => {
+        return expect(rational2.multiplicacion(3,5,1,2)).to.be.equal(undefined);
     })
 });
